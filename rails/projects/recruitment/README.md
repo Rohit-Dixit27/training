f1629cfd9991e2fef9dbf8c5dc827edb974c2f40
Rohit Dixit <rohitdxt1999@gmail.com>
2023-03-13 17:21:51 +0530

    layouts in action mailer

--- rails/projects/recruitment/README.md
+++ rails/projects/recruitment/README.md
@@ -7,3 +7,7 @@
 
 --template_path
 -we can specify template path and template name also.
+
+--Layout
+-Just like with controller views, use yield to render the view inside the layout.
+-You can also pass in a layout: 'layout_name' option to the render call inside the format block to specify different layouts for different formats
--- rails/projects/recruitment/app/mailers/notifier_mailer.rb
+++ rails/projects/recruitment/app/mailers/notifier_mailer.rb
@@ -1,7 +1,7 @@
 class NotifierMailer < ApplicationMailer
     def send_notification
         @user = params[:user]
-         mail(to:"rohitdxt1999@gmail.com", from:"sharmarick59@gmail.com", subject:"this is data",
+         mail(to: email_address_with_name(@user.email, @user.name), from:"sharmarick59@gmail.com", subject:"this is data",
         template_path: "notification", template_name: "notification")    
     end
 end
