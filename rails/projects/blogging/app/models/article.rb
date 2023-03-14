3124589a11160eae1d7b95c1f0b67193d592a038
Rohit Dixit <rohitdxt1999@gmail.com>
2023-03-14 18:15:20 +0530

    adding multiple files in active storage

--- rails/projects/blogging/app/models/article.rb
+++ rails/projects/blogging/app/models/article.rb
@@ -1,7 +1,3 @@
 class Article < ApplicationRecord
-    has_one_attached :image
-
-    def image_modify
-        image.variant(resize_to_limit: [200, 200]).processed
-    end
+    has_one_attached :file
 end
