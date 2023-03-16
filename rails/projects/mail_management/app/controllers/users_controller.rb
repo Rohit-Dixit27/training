19e392f251b1168f8cb515f210f59099e8de459d
Rohit Dixit <rohitdxt1999@gmail.com>
2023-03-16 18:59:53 +0530

    fix support only excel files

--- rails/projects/mail_management/app/controllers/users_controller.rb
+++ rails/projects/mail_management/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    #@users = User.all
     @users = User.paginate(page: params[:page], per_page: 2)
   end
 
@@ -12,20 +12,18 @@ class UsersController < ApplicationController
 
   def import_from_excel
     file = params[:file]
-    begin
-    # Retreive the extension of the file
     file_ext = File.extname(file.original_filename)
-    raise "Unknown file type: #{file.original_filename}" unless [".xls", ".xlsx"].include?(file_ext)
-    spreadsheet = (file_ext == ".xls") ? Roo::Excel.new(file.path) : Roo::Excelx.new(file.path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-    User.create(sno: spreadsheet.row(i)[0], name: spreadsheet.row(i)[1], email: spreadsheet.row(i)[2])
-    end
-    flash[:notice] = "Records Imported"
-    redirect_to root_path 
-    rescue Exception => e
+
+    if User::ALLOWED_EXTENSIONS.include?(file_ext)
+      spreadsheet = (file_ext == ".xls") ? Roo::Excel.new(file.path) : Roo::Excelx.new(file.path)
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+      User.create(sno: spreadsheet.row(i)[0], name: spreadsheet.row(i)[1], email: spreadsheet.row(i)[2])
+      end
+      flash[:notice] = "imported"
+    else
       flash[:notice] = "supports only excel files"
-      redirect_to root_path 
     end
-    end
+    redirect_to root_path
+  end
 end
--- rails/projects/mail_management/app/models/user.rb
+++ rails/projects/mail_management/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, uniqueness: true
   after_create :send_email
-
+  ALLOWED_EXTENSIONS = ['.xls', '.xlsx']
+  
   def send_email
     NotifierMailer.with(user: self).delay.welcome
   end
