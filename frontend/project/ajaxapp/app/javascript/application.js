8432bef1b1a6d70e20508cd815c952e410749b48
Rohit Dixit <rohitdxt1999@gmail.com>
2023-03-30 18:18:17 +0530

    fixes sorting using ajax

--- frontend/project/ajaxapp/app/javascript/application.js
+++ frontend/project/ajaxapp/app/javascript/application.js
@@ -6,12 +6,17 @@ import "controllers"
 //= require jquery_ujs
 
   $(function() {
-    $(".sort_paginate_ajax").on("click", ".pagination a", function(){
+    $(".sort_paginate_ajax ").on("click", ".pagination a", function(){
     $.getScript(this.href);
       return false;
     });
-  });
+ 
   $('#users_search').submit(function(){
     $.get(this.action, $(this).serialize(), null, "script")
+  });
+  $(".sort_paginate_ajax ").on("click","th a",  function(){
+    $.getScript(this.href);
+      return false;
+    });
   });
 
