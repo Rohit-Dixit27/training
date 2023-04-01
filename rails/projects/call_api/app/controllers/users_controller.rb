1bf903b5f0fe4c448773e88e5f2e55ae25a0ef31
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-01 13:58:34 +0530

    adds devise authentication

--- rails/projects/call_api/app/controllers/users_controller.rb
+++ rails/projects/call_api/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_admin!
   def index
     response = HTTParty.get("https://api.publicapis.org/categories")
     @response = JSON.parse(response.body)
