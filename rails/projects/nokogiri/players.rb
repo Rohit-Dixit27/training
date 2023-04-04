9c45a03a06fe198e3f10e70fa70f160941928155
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-04 19:01:37 +0530

    finding playing eleven from both the teams

--- rails/projects/nokogiri/players.rb
+++ rails/projects/nokogiri/players.rb
@@ -4,10 +4,28 @@ require 'open-uri'
 
 data =  URI.open("https://www.cricbuzz.com/cricket-match-squads/66197/csk-vs-lsg-6th-match-indian-premier-league-2023/").read
 object = Nokogiri::HTML.parse(data)
-object.xpath("//div[@class='cb-col cb-col-100']/a/div").each do |i|
-  puts i.text
+
+arr1 = Array.new();
+count1=0;
+object.xpath("//div[@class='cb-player-name-right']").each do |i|
+  arr1.append(i.text)
+  count1+=1
+  if(count1>10)
+    break
+  end
 end
- 
+puts arr1
+
+arr2 = Array.new();
+count2=0;
+object.xpath("//div[@class='cb-player-name-left']").each do |i|
+  arr2.append(i.text)
+  count2+=1
+  if(count2>10)
+    break
+  end
+end
+puts arr2
 
 
 
