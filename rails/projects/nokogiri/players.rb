ee6343f6c327559acddbc7716850c8985e2411f2
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-05 10:33:08 +0530

    finding team1 and team2

--- rails/projects/nokogiri/players.rb
+++ rails/projects/nokogiri/players.rb
@@ -5,27 +5,29 @@ require 'open-uri'
 data =  URI.open("https://www.cricbuzz.com/cricket-match-squads/66197/csk-vs-lsg-6th-match-indian-premier-league-2023/").read
 object = Nokogiri::HTML.parse(data)
 
-arr1 = Array.new();
-count1=0;
-object.xpath("//div[@class='cb-player-name-right']").each do |i|
-  arr1.append(i.text)
-  count1+=1
-  if(count1>10)
+player_of_team1 = Array.new();
+count_team1=0;
+object.xpath("//div[@class='cb-player-name-left']").each do |player|
+  player_of_team1.append(player.text)
+  count_team1+=1
+  if(count_team1>10)
     break
   end
 end
-puts arr1
+puts "Team1"
+puts player_of_team1
 
-arr2 = Array.new();
-count2=0;
-object.xpath("//div[@class='cb-player-name-left']").each do |i|
-  arr2.append(i.text)
-  count2+=1
-  if(count2>10)
+player_of_team2 = Array.new();
+count_team2=0;
+object.xpath("//div[@class='cb-player-name-right']").each do |player|
+  player_of_team2.append(player.text)
+  count_team2+=1
+  if(count_team2>10)
     break
   end
 end
-puts arr2
+puts "Team2"
+puts player_of_team2
 
 
 
