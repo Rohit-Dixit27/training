5e706e12cc884920ca1ef1492ee045c450fcb662
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-05 14:28:35 +0530

    finding footer elements

--- rails/projects/nokogiri/players.rb
+++ rails/projects/nokogiri/players.rb
@@ -5,8 +5,14 @@ require 'open-uri'
 data =  URI.open("https://www.cricbuzz.com/cricket-match-squads/66197/csk-vs-lsg-6th-match-indian-premier-league-2023").read
 doc = Nokogiri::HTML.parse(data)
 
-doc.xpath("//nav[@class='cb-nav-bar']/a").each do |nav_element|
-  puts nav_element.text
+# finding headline
+doc.xpath("//div[@class='cb-billing-plans-text cb-team-lft-item']/h1").each do |headline|
+  puts headline.text
+end
+
+# finding footer
+doc.xpath("//div[@class='cb-mr-top-10 cb-guide-item']/div").each do |footer|
+  puts footer.text
 end
 
 
