6ff1c0c15eb2f623915705d3239b850a7a9c01cf
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-07 14:30:35 +0530

    automatically search for given text

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
+require 'webdrivers'
 require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :chrome
-driver.navigate().to("https://www.ebay.com/")
+
+driver.get("https://www.ebay.com/")
+driver.find_element(:id, "gh-ac").send_keys("JBL")
+sleep 2
+driver.find_element(:id, "gh-btn").click
+sleep 2
 puts "done navigation"
