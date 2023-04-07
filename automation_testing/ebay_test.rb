c77c653f678a07301114fc29326fb33493dfed23
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-07 16:09:28 +0530

    facebook login

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -4,9 +4,11 @@ require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :chrome
 
-driver.get("https://www.ebay.com/")
-driver.find_element(:id, "gh-ac").send_keys("JBL")
+driver.get("https://www.facebook.com/")
+driver.find_element(:id, "email").send_keys("rohitdxt1999@gmail.com")
 sleep 2
-driver.find_element(:id, "gh-btn").click
+driver.find_element(:id, "pass").send_keys("password")
 sleep 2
+driver.find_element(:name, "login").click
+sleep 10
 puts "done navigation"
