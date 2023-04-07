c42cedeaf2447ffb2b8de13bb1de34e2299d5d14
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-07 18:40:23 +0530

    test click on links, navigate, dropdown

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -5,10 +5,17 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
 
 driver.get("https://www.facebook.com/")
-driver.find_element(:id, "email").send_keys("rohitdxt1999@gmail.com")
 sleep 2
-driver.find_element(:id, "pass").send_keys("password")
-sleep 2
-driver.find_element(:name, "login").click
-sleep 10
-puts "done navigation"
+driver.find_element(:xpath, "//a[text() = 'Forgotten password?']").click
+sleep 1
+driver.navigate().to("https://www.w3schools.com/howto/howto_css_dropdown_navbar.asp")
+driver.find_element(:id, "navbtn_tutorials").click
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Learn CSS']").click
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Start CSS Quiz!']").click
+sleep 1
+driver.navigate().to("https://www.w3schools.com/cssref/sel_hover.php")
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Try it Yourself »']").click
+sleep 1
