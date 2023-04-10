289c24d0e2f85c9936c3385fb0c31b58e169000a
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-10 17:24:32 +0530

    forward, backward and refresh window

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -4,28 +4,17 @@ require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :chrome
 
-driver.get("https://www.facebook.com/")
+driver.get("https://www.google.com/")
 driver.manage.window.maximize
+search = driver.find_element(:name, 'q')
+search.send_keys("selenium")
 sleep 2
-driver.find_element(:xpath, "//a[text() = 'Forgotten password?']").click
-sleep 1
-driver.navigate().to("https://www.w3schools.com/howto/howto_css_dropdown_navbar.asp")
-driver.find_element(:id, "navbtn_tutorials").click
-sleep 1
-driver.find_element(:xpath, "//a[text() = 'Learn CSS']").click
-sleep 1
-driver.find_element(:xpath, "//a[text() = 'Start CSS Quiz!']").click
-sleep 1
-driver.navigate().to("https://www.w3schools.com/cssref/sel_hover.php")
-sleep 1
-driver.find_element(:xpath, "//a[text() = 'Try it Yourself »']").click
-sleep 1
-driver.switch_to.window(driver.window_handles[1])
-driver.navigate.to("http://localhost:3000/")
-driver.find_element(:id, 'age_child').click
-driver.find_element(:id, 'age_adult').click
-driver.find_element(:id, 'read_book').click
-driver.find_element(:id, 'add_book').click
+search.submit()
+driver.navigate.to("https://www.facebook.com/")
 sleep 2
-
+driver.navigate.back
+driver.navigate.refresh
+sleep 2
+driver.navigate.forward
+sleep 2
 
