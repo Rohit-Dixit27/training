b54ed62a4276b307b876c0d7c8fb397a6d0b742f
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-10 11:51:25 +0530

    hover on all nav_links in ebay

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -6,10 +6,46 @@ action = Selenium::WebDriver::ActionBuilder
 
 driver = Selenium::WebDriver.for :chrome
 
+# driver.get("https://www.ebay.com/")
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Electronics']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//a[text() = 'Motors']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Fashion']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Collectibles and Art']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Sports']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Health & Beauty']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Industrial equipment']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Home & Garden']")
+# driver.action.move_to(hover).perform
+# sleep 2
+# hover = driver.find_element(:xpath, "//li//a[text() = 'Sell']")
+# driver.action.move_to(hover).perform
+# sleep 2
+
+
+all_navbar_links = ['Electronics', 'Motors', 'Fashion', 'Collectibles and Art', 'Sports', 'Health & Beauty', 'Industrial equipment', 'Home & Garden', 'Sell']
 driver.get("https://www.ebay.com/")
-hover = driver.find_element(:xpath, "//a[text() = 'Motors']")
-driver.action.move_to(hover).perform
-sleep 2
+driver.manage.window.maximize
+nav_links=0
+while nav_links<9
+  hover = driver.find_element(:xpath, "//li//a[text() = '#{all_navbar_links[nav_links]}']")
+  driver.action.move_to(hover).perform
+  sleep 2
+  nav_links+=1
+end
 
 
 
