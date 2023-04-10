dbac529952ecf7117c6db3c06699548ab81537fd
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-10 12:38:30 +0530

    testing radio buttons and checkboxes

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -3,49 +3,13 @@ require 'webdrivers'
 require 'selenium-webdriver'
 action = Selenium::WebDriver::ActionBuilder
 
-
 driver = Selenium::WebDriver.for :chrome
-
-# driver.get("https://www.ebay.com/")
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Electronics']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//a[text() = 'Motors']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Fashion']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Collectibles and Art']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Sports']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Health & Beauty']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Industrial equipment']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Home & Garden']")
-# driver.action.move_to(hover).perform
-# sleep 2
-# hover = driver.find_element(:xpath, "//li//a[text() = 'Sell']")
-# driver.action.move_to(hover).perform
-# sleep 2
-
-
-all_navbar_links = ['Electronics', 'Motors', 'Fashion', 'Collectibles and Art', 'Sports', 'Health & Beauty', 'Industrial equipment', 'Home & Garden', 'Sell']
-driver.get("https://www.ebay.com/")
-driver.manage.window.maximize
-nav_links=0
-while nav_links<9
-  hover = driver.find_element(:xpath, "//li//a[text() = '#{all_navbar_links[nav_links]}']")
-  driver.action.move_to(hover).perform
-  sleep 2
-  nav_links+=1
-end
+driver.get("http://localhost:3000/")
+driver.find_element(:id, 'age_child').click
+driver.find_element(:id, 'age_adult').click
+driver.find_element(:id, 'read_book').click
+driver.find_element(:id, 'add_book').click
+sleep 2
 
 
 
