2e745e2bf22915d6fe1a49012e03be1d96b50de7
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-10 14:37:09 +0530

    switch to windows

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -1,15 +1,31 @@
 require 'rubygems'
 require 'webdrivers'
 require 'selenium-webdriver'
-action = Selenium::WebDriver::ActionBuilder
 
 driver = Selenium::WebDriver.for :chrome
-driver.get("http://localhost:3000/")
+
+driver.get("https://www.facebook.com/")
+driver.manage.window.maximize
+sleep 2
+driver.find_element(:xpath, "//a[text() = 'Forgotten password?']").click
+sleep 1
+driver.navigate().to("https://www.w3schools.com/howto/howto_css_dropdown_navbar.asp")
+driver.find_element(:id, "navbtn_tutorials").click
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Learn CSS']").click
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Start CSS Quiz!']").click
+sleep 1
+driver.navigate().to("https://www.w3schools.com/cssref/sel_hover.php")
+sleep 1
+driver.find_element(:xpath, "//a[text() = 'Try it Yourself »']").click
+sleep 1
+driver.switch_to.window(driver.window_handles[1])
+driver.navigate.to("http://localhost:3000/")
 driver.find_element(:id, 'age_child').click
 driver.find_element(:id, 'age_adult').click
 driver.find_element(:id, 'read_book').click
 driver.find_element(:id, 'add_book').click
 sleep 2
-
 
 
