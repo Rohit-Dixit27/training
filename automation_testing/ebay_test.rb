974bcf5405d0b60eabf16afef2244abbf1b9028a
Rohit Dixit <rohitdxt1999@gmail.com>
2023-04-11 14:52:24 +0530

    implicit wait

--- automation_testing/ebay_test.rb
+++ automation_testing/ebay_test.rb
@@ -4,10 +4,12 @@ require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :chrome
 
+# throw exception after 10 seconds
+driver.manage.timeouts.implicit_wait = 10
 driver.get("http://127.0.0.1:5500/index.html")
 driver.manage.window.maximize
 elements = Array.new()
-elements = driver.find_elements(:id, 'th')
+elements = driver.find_elements(:id, 't')
 puts elements.size
 i=0
 
